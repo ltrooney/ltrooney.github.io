927d793c23262c974d9ce7082b51d0055c094c0a
Luke Rooney <ltrooney@berkeley.edu>
2019-03-24 10:16:03 -0700

    refactored projects json

--- js/script.js
+++ js/script.js
@@ -44,60 +44,98 @@ jQuery(document).ready(function($) {
 
 	var json = 
 		{
-			projects: {
-				previews: [
-					{ id: "01", title: "Autonomous Quadcopter",	dates: "Jul 2017 - Jan 2019",	imgSrc: "https://graysonhobby.com/media/catalog/category/FPV-Drone-Racer.jpg"},
-					{ id: "02", title: "Music Theory App",		dates: "Jul 2017 - Jan 2019",	imgSrc: "css/images/music.png" },
-					{ id: "03", title: "Hack Computer",			dates: "Jul 2017 - Jan 2019",	imgSrc: "https://onlinejpgtools.com/images/examples-onlinejpgtools/random-grid.jpg" },
-					{ id: "04", title: "JAVA Physics Engine",	dates: "Jul 2017 - Jan 2019",	imgSrc: "css/images/physics.jpg" },
-					{ id: "05", title: "Pacman AI Agent",		dates: "January 2019", 			imgSrc: "https://androidapksfree.com/wp-content/uploads/2016/12/PAC-MAN-apk.png"},
-					{ id: "06", title: "This Website",			dates: "Dec 2018 - Mar 2019", 	imgSrc: "css/images/code.jpeg"}
-				],
-				modals: [
-					{ 
-						id: "01",
-						title: "Autonomous Quadcopter",
+			projects: [
+				{
+					enabled: true,
+					id: "01",
+					title: "Autonomous Quadcopter",
+					preview: {
+						dates: "Jul 2017 - Jan 2019",
+						imgSrc: "https://graysonhobby.com/media/catalog/category/FPV-Drone-Racer.jpg"
+					},
+					modal: {
+						isPrivate: false,
 						githubLink: "github.com/ltrooney/quadcopter",
 						skills: "C++, Arduino, PID Controller, Motors, Power Systems",
 						description: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
+					}	
+				},
+				{
+					enabled: true,
+					id: "02",
+					title: "Music Theory App",
+					preview: {
+						dates: "Jul 2017 - Jan 2019",
+						imgSrc: "css/images/music.png"
 					},
-					{ 
-						id: "02",
-						title: "Music Theory App",
+					modal: {
+						isPrivate: false,
 						githubLink: "github.com/ltrooney/music_theory_app",
 						skills: "C++, Arduino, PID Controller, Motors, Power Systems",
 						description: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
+					}	
+				},
+				{
+					enabled: true,
+					id: "03",
+					title: "Hack Computer",
+					preview: {
+						dates: "Jul 2017 - Jan 2019",
+						imgSrc: "https://onlinejpgtools.com/images/examples-onlinejpgtools/random-grid.jpg"
 					},
-					{ 
-						id: "03",
-						title: "Hack Computer",
+					modal: {
+						isPrivate: false,
 						githubLink: "github.com/ltrooney/hack-computer",
 						skills: "C++, Arduino, PID Controller, Motors, Power Systems",
 						description: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
+					}	
+				},
+				{
+					enabled: true,
+					id: "04",
+					title: "JAVA Physics Engine",
+					preview: {
+						dates: "Jul 2017 - Jan 2019",
+						imgSrc: "css/images/physics.jpg"
 					},
-					{ 
-						id: "04",
-						title: "JAVA Physics Engine",
+					modal: {
+						isPrivate: false,
 						githubLink: "github.com/ltrooney/physics-engine",
-						skills: "Java",
+						skills: "C++, Arduino, PID Controller, Motors, Power Systems",
 						description: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
+					}	
+				},
+				{
+					enabled: true,
+					id: "05",
+					title: "Pacman AI Agent",
+					preview: {
+						dates: "January 2019",
+						imgSrc: "https://androidapksfree.com/wp-content/uploads/2016/12/PAC-MAN-apk.png"
 					},
-					{ 
-						id: "05",
-						title: "Pacman AI Agent",
+					modal: {
+						isPrivate: true,
 						githubLink: null,
-						skills: "Python",
+						skills: "Python, Approximate Q-Learning, A* Search, Dijkstra's Algorithm",
 						description: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
+					}	
+				},
+				{
+					enabled: true,
+					id: "06",
+					title: "This Website",
+					preview: {
+						dates: "Dec 2018 - Mar 2019",
+						imgSrc: "css/images/code.jpeg"
 					},
-					{ 
-						id: "06",
-						title: "This Website",
+					modal: {
+						isPrivate: false,
 						githubLink: "github.com/ltrooney/luke-rooney-website",
-						skills: "HTML/CSS, JavaScript",
+						skills: "C++, Arduino, PID Controller, Motors, Power Systems",
 						description: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus."
-					}
-				]
-			},
+					}	
+				},
+			],
 			experience: [
 				{ 
 					imgSrc: "css/images/jpl.jpg",
@@ -136,66 +174,77 @@ jQuery(document).ready(function($) {
 			]
 		};
 
+	var modalsEnabled = true;
 	var modalPrefix = "project-modal";
 
 	// parse data for projects section
 	var projectsJSON = json.projects;
-  	projectsJSON.previews.forEach(function(previewData) {
+  	projectsJSON.forEach(function(projectsData) {
   		var html = 
   		`<div class='single_portfolio_text mx-2'>
-			<img src="${previewData.imgSrc}" />
+			<img src="${projectsData.preview.imgSrc}" />
 			<div class='portfolio_images_overlay text-center my-auto'>
 				<div class='space-header-wrapper d-flex align-items-center'>
-					<h6 class='space-header gallery-text'>${previewData.title}</h6>
+					<h6 class='space-header gallery-text'>${projectsData.title}</h6>
 				</div>
-				<p class='dates'>${previewData.dates}</p>
-				<button type='button' class='btn btn-dark btn-lg' data-toggle='modal' data-target='#${modalPrefix}-${previewData.id}'>View</button>
-			</div>
-		</div>"`;
+				<p class='dates'>${projectsData.preview.dates}</p>`;
+				if (modalsEnabled) {
+					html += `<button type='button' class='btn btn-dark btn-lg' data-toggle='modal' data-target='#${modalPrefix}-${projectsData.id}'>View</button>`;
+				}
+			html += `</div></div>"`;
 
   		$('#project-previews').append(html);
+
+  		if (modalsEnabled) {
+  			var modalData = projectsData.modal;
+  			console.log(modalData);
+	  		var modal_html = 
+	  		`<div class="modal fade" id="${modalPrefix}-${projectsData.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		        <div class="modal-dialog modal-dialog-centered" role="document">
+		          <div class="modal-content">
+		            <div class="modal-header">
+		              <h5 class="modal-title" id="exampleModalLabel">${projectsData.title}</h5>
+		              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		                <span aria-hidden="true">&times;</span>
+		              </button>
+		            </div>
+		            <div class="modal-body">`
+
+		            if (modalData.isPrivate) {
+		            	modal_html += 
+		            	`<div class="project-section">
+			                <h6>Project Links</h6>
+			            	<p>(Private Source)</p>
+			             </div>`;
+		            } else if (modalData.githubLink != null) {
+		            	modal_html += 
+		            	`<div class="project-section">
+			                <h6>Project Links</h6>
+			            	<a href="https://${modalData.githubLink}"><i class="fab fa-github mr-2"></i>${modalData.githubLink}</a>
+			             </div>`;
+		            }
+		             
+		            modal_html +=  
+		              `<div class="project-section">
+		                <h6>Skills</h6>
+		                <p>${modalData.skills}</p>
+		              </div>
+		              <div class="project-section">
+		                <h6>Description</h6>
+		                <p>${modalData.description}</p>
+		              </div>
+		            </div>
+		            <div class="modal-footer justify-content-centerw">
+		              <button type="button" class="btn btn-dark" data-dismiss="modal">Close</button>
+		            </div>
+		          </div>
+		        </div>
+		      </div>`;
+	  		$('#projects').append(modal_html);
+  		}
   	});
 
-  	projectsJSON.modals.forEach(function(modalData) {
-  		var html = 
-  		`<div class="modal fade" id="${modalPrefix}-${modalData.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-	        <div class="modal-dialog modal-dialog-centered" role="document">
-	          <div class="modal-content">
-	            <div class="modal-header">
-	              <h5 class="modal-title" id="exampleModalLabel">${modalData.title}</h5>
-	              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-	                <span aria-hidden="true">&times;</span>
-	              </button>
-	            </div>
-	            <div class="modal-body">`
-
-	            if (modalData.githubLink != null) {
-	            	html += 
-	            	`<div class="project-section">
-		                <h6>Project Links</h6>
-		            	<a href="https://${modalData.githubLink}"><i class="fab fa-github mr-2"></i>${modalData.githubLink}</a>
-		             </div>`;
-	            }
-	             
-	            html +=  
-	              `<div class="project-section">
-	                <h6>Skills</h6>
-	                <p>${modalData.skills}</p>
-	              </div>
-	              <div class="project-section">
-	                <h6>Description</h6>
-	                <p>${modalData.description}</p>
-	              </div>
-	            </div>
-	            <div class="modal-footer justify-content-centerw">
-	              <button type="button" class="btn btn-dark" data-dismiss="modal">Close</button>
-	            </div>
-	          </div>
-	        </div>
-	      </div>`;
-  		$('#projects').append(html);
-  	});
-
+  	
   	json.experience.forEach(function(experienceData) {
   		var html = 
   		`<div class="row mb-4">
@@ -206,7 +255,7 @@ jQuery(document).ready(function($) {
               </div>
               <div class="col-lg-8 col-md-8 col-sm-12">
                 <h4><strong>${experienceData.institution}</strong></h4>
-                <h6>${experienceData.title}</h6>
+                <h6>${experienceData.jobTitle}</h6>
                 <i>${experienceData.dates}</i>
                 <p class="mt-2">${experienceData.description}</p>
               </div>
@@ -215,6 +264,7 @@ jQuery(document).ready(function($) {
         </div>`;
         $('#experiences').append(html);
   	});
+  	
 
   	json.faqs.forEach(function(faq) {
   		var html =
